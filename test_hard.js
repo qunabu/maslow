170f8ceb24e3bb9b2690dedaf9803fa66f5c15b0
Mateusz Qunabu <mateusz@qunabu.com>
2019-04-18 16:10:11 +0200

    Update test_hard.js

--- test_hard.js
+++ test_hard.js
@@ -4,9 +4,9 @@ const mcp3 = require('simple-mcp3008')(18,24,23,25,3.3);
 
 function loop() {
 	var i;
-	for (i=0;i<7;i++) { console.log('mcp1', i+1, mcp1.pins[i].getDecimalValue()); }
-	for (i=0;i<7;i++) { console.log('mcp2', i+1, mcp2.pins[i].getDecimalValue()); }
-	for (i=0;i<7;i++) { console.log('mcp3', i+1, mcp3.pins[i].getDecimalValue()); }
+	for (i=0;i<=7;i++) { console.log('mcp1', i+1, mcp1.pins[i].getDecimalValue()); }
+	for (i=0;i<=7;i++) { console.log('mcp2', i+1, mcp2.pins[i].getDecimalValue()); }
+	for (i=0;i<=7;i++) { console.log('mcp3', i+1, mcp3.pins[i].getDecimalValue()); }
 }
 
 setInterval(loop, 500);
